bff7d7d53b6473293ea48ce98f5da9e62d483d98
lorgar-avrelian <victor-14-244@mail.ru>
2025-04-21 08:39:48 +0300

    In progress

--- core/src/main/java/lorgar/avrelian/core/controller/CoreController.java
+++ core/src/main/java/lorgar/avrelian/core/controller/CoreController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
